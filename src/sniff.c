090854bd7c7c2e88008ef2bfd182a63d03945c0d
hellcat <hellcat@88.com>
2022-01-11 13:02:12 +0800

    Using pcap_loop() to capture packet

--- src/sniff.c
+++ src/sniff.c
@@ -10,7 +10,7 @@
 struct arguments {
     pcap_t *pcap_handle;
     int verbose;
-};
+}; // check
 
 void packet_handler(u_char *args, const struct pcap_pkthdr *header,
                     const u_char *packet) {
